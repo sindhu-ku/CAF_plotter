50216747ec9f67bfeb8688daed3aa4f1c6c72778
sindhu-ku <kumaran.sindhujha@gmail.com>
2023-08-30 19:46:23 -0500

    remove truth stuff for now

--- ApplyCuts.cc
+++ ApplyCuts.cc
@@ -2,6 +2,5 @@
 
 bool ApplyCuts(const caf::StandardRecord* sr, int nixn, int npart){
 	bool contained = sr->common.ixn.dlp[nixn].part.dlp[npart].contained;
-	bool iscc = sr->mc.nu[nixn].iscc;
-	return (!contained && iscc);
+	return (!contained);
 }
