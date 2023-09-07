c90419c6cc488fac66fcad39e6bfcb23113c79fe
sindhu-ku <kumaran.sindhujha@gmail.com>
2023-09-07 16:48:05 -0500

    add plots folder

--- main.cc
+++ main.cc
@@ -1,15 +1,16 @@
 #include "CAFPlotter.h"
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cout << "\n USAGE: " << argv[0] << " input_caf_file_list output_root_file\n" << std::endl;
+    if (argc != 4) {
+        std::cout << "\n USAGE: " << argv[0] << " input_caf_file_list output_root_file output_plots_folder\n" << std::endl;
         return 1;
     }
 
     std::string input_file_list = argv[1];
     std::string output_rootfile = argv[2];
+    std::string output_plots_folder = argv[3];
 
-    CAFPlotter caf_plotter(input_file_list, output_rootfile);
+    CAFPlotter caf_plotter(input_file_list, output_rootfile, output_plots_folder);
     caf_plotter.process();
 
     return 0;
