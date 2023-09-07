6f4cc9f7d9b0641485d9f075b8e73fdc58dead61
sindhu-ku <kumaran.sindhujha@gmail.com>
2023-09-07 16:46:37 -0500

    set branch status, but does not work now

--- TreeLoader.cc
+++ TreeLoader.cc
@@ -2,6 +2,13 @@
 #include <fstream>
 #include <iostream>
 
+void TreeLoader::SetBranchStatus(TChain* chain){
+
+    chain->SetBranchStatus("*", 0);
+    chain->SetBranchStatus("rec.common.ixn.dlp", 1);
+    chain->SetBranchStatus("rec.nd.lar.dlp.tracks", 1);
+}
+
 TChain* TreeLoader::loadTree(const std::string& input_file_list) {
     TChain* caf_chain = new TChain("cafTree");
 
@@ -15,7 +22,6 @@ TChain* TreeLoader::loadTree(const std::string& input_file_list) {
     std::string tmp;
     while (caf_list >> tmp) {
         caf_chain->Add(tmp.c_str());
-        std::cout << "Adding File " << tmp << std::endl;
     }
 
     if (!caf_chain || caf_chain->GetEntries() == 0) {
@@ -25,6 +31,10 @@ TChain* TreeLoader::loadTree(const std::string& input_file_list) {
     }
 
     caf_list.close();
+    
+    //Not able to activate ixn branch, not using for now
+    //SetBranchStatus(caf_chain); //Activate just the branches needed. 
+    
     return caf_chain;
 }
 
--- TreeLoader.h
+++ TreeLoader.h
@@ -7,6 +7,7 @@
 class TreeLoader {
 public:
     static TChain* loadTree(const std::string& input_file_list);
+    static void SetBranchStatus(TChain* chain);
 };
 
 #endif // TREELOADER_H
