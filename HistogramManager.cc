099946b1eb6c6b8116a3cc548c6d4c8ed9df7fad
sindhu-ku <kumaran.sindhujha@gmail.com>
2023-09-07 16:46:19 -0500

    Add plotting, rm some unecessary hists

--- HistogramManager.cc
+++ HistogramManager.cc
@@ -1,35 +1,43 @@
 #include "HistogramManager.h"
 #include "TFile.h"
+#include "TStyle.h"
+#include <filesystem>
 
 HistogramCollection HistogramManager::defineHistograms() {
     HistogramCollection histograms;
     
     histograms.part_energy = new TH1D("part_energy","Reconstructed particle energy in GeV", 100, 0, 1);
-    histograms.interaction_vtx_R = new TH1D("interaction_vtx_R", "Reconstructed interaction vertex R [cm]", 200, 0, 200);
-    histograms.interaction_vtx_x = new TH1D("interaction_vtx_x", "Reconstructed interaction vertex x [cm]", 400, -200, 200);
-    histograms.interaction_vtx_y = new TH1D("interaction_vtx_y", "Reconstructed interaction vertex y [cm]", 400, -200, 200);
-    histograms.interaction_vtx_z = new TH1D("interaction_vtx_z", "Reconstructed interaction vertex z [cm]", 400, -200, 200);
+    histograms.interaction_vtx_x = new TH1D("interaction_vtx_x", "Reconstructed interaction vertex x [cm]", 35, -70, 70);
+    histograms.interaction_vtx_y = new TH1D("interaction_vtx_y", "Reconstructed interaction vertex y [cm]", 100, -400, -200);
+    histograms.interaction_vtx_z = new TH1D("interaction_vtx_z", "Reconstructed interaction vertex z [cm]", 100, 1200, 1400);
     histograms.tracks_Ecalo_len = new TH2D("tracks_Ecalo_len", "Reconstructed tracks calorimetric energy (GeV) vs track length [cm]", 100, 0, 1, 200, 0, 200);    
-    histograms.tracks_Erange_len = new TH2D("tracks_Erange_len", "Reconstructed tracks range-based energy (GeV) vs track length [cm]", 100, 0, 1, 200, 0, 200);    
 
     return histograms;
 }
-
+//to do: titles, plot styles should be done better, too repetitive now
 void HistogramManager::fillRecoIxnHistograms(HistogramCollection& histograms, const caf::StandardRecord* sr, int nixn){
-    histograms.interaction_vtx_R->Fill(sqrt(pow(sr->common.ixn.dlp[nixn].vtx.x,2) + pow(sr->common.ixn.dlp[nixn].vtx.y,2) + pow(sr->common.ixn.dlp[nixn].vtx.z,2)));
     histograms.interaction_vtx_x->Fill(sr->common.ixn.dlp[nixn].vtx.x);
     histograms.interaction_vtx_y->Fill(sr->common.ixn.dlp[nixn].vtx.y);
     histograms.interaction_vtx_z->Fill(sr->common.ixn.dlp[nixn].vtx.z);
+    histograms.interaction_vtx_x->GetXaxis()->SetTitle("Reco. interaction vertex x [cm]");
+    histograms.interaction_vtx_x->GetYaxis()->SetTitle("No. of interactions");
+    histograms.interaction_vtx_y->GetXaxis()->SetTitle("Reco. interaction vertex y [cm]");
+    histograms.interaction_vtx_y->GetYaxis()->SetTitle("No. of interactions");
+    histograms.interaction_vtx_z->GetXaxis()->SetTitle("Reco. interaction vertex z [cm]");
+    histograms.interaction_vtx_z->GetYaxis()->SetTitle("No. of interactions");
 
 }
 
 void HistogramManager::fillRecoPartHistograms(HistogramCollection& histograms, const caf::StandardRecord* sr, int nixn, int npart){
     histograms.part_energy->Fill(sr->common.ixn.dlp[nixn].part.dlp[npart].E);
+    histograms.part_energy->GetXaxis()->SetTitle("Reco. particle energy [GeV]");
+    histograms.part_energy->GetYaxis()->SetTitle("No. of particles");
 
 }
 void HistogramManager::fillRecoTracksHistograms(HistogramCollection& histograms, const caf::StandardRecord* sr, int nixn, int ntrack){
     histograms.tracks_Ecalo_len->Fill(sr->nd.lar.dlp[nixn].tracks[ntrack].Evis, sr->nd.lar.dlp[nixn].tracks[ntrack].len_cm);
-    histograms.tracks_Erange_len->Fill(sr->nd.lar.dlp[nixn].tracks[ntrack].E, sr->nd.lar.dlp[nixn].tracks[ntrack].len_cm);
+    histograms.tracks_Ecalo_len->GetXaxis()->SetTitle("Track length [cm]");
+    histograms.tracks_Ecalo_len->GetYaxis()->SetTitle("Calorimetric energy [GeV]");
 
 }
 
@@ -37,11 +45,45 @@ void HistogramManager::fillRecoTracksHistograms(HistogramCollection& histograms,
 void HistogramManager::writeHistograms(const HistogramCollection& histograms, const std::string& output_rootfile) {
     TFile* caf_out_file = new TFile(output_rootfile.c_str(), "recreate");
     histograms.part_energy->Write();
-    histograms.interaction_vtx_R->Write();
     histograms.interaction_vtx_x->Write();
     histograms.interaction_vtx_y->Write();
     histograms.interaction_vtx_z->Write();
     histograms.tracks_Ecalo_len->Write();
-    histograms.tracks_Erange_len->Write();
     caf_out_file->Close();
 }
+
+void HistogramManager::plotHistograms(const HistogramCollection& histograms, const std::string& output_folder) {
+    if (!std::filesystem::exists(output_folder.c_str())) std::filesystem::create_directory(output_folder.c_str());
+
+   
+    gStyle->SetLabelSize(0.05, "X");
+    gStyle->SetLabelSize(0.05, "Y");
+    gStyle->SetTitleSize(0.06, "X");
+    gStyle->SetTitleSize(0.06, "Y");
+    
+    TCanvas *c =  new TCanvas("c", "c", 800, 600);
+    gStyle->SetOptStat(0);
+ 
+    plot1DHistogram(histograms.part_energy, output_folder, c);    
+    plot1DHistogram(histograms.interaction_vtx_x, output_folder, c);    
+    plot1DHistogram(histograms.interaction_vtx_y, output_folder, c);    
+    plot1DHistogram(histograms.interaction_vtx_z, output_folder, c);    
+    plot2DHistogram(histograms.tracks_Ecalo_len, output_folder, c);    
+    
+}
+
+void HistogramManager::plot1DHistogram(TH1D* hist, const std::string& output_folder, TCanvas* c) {
+
+    hist->SetLineWidth(2);
+    hist->Draw();
+    std::string output_pngfile = output_folder + "/" + hist->GetName() + ".png";
+    c->SaveAs(output_pngfile.c_str());
+}
+
+void HistogramManager::plot2DHistogram(TH2D* hist, const std::string& output_folder, TCanvas* c) {
+
+    hist->Draw("colz");
+    std::string output_pngfile = output_folder + "/" + hist->GetName() + ".png";
+    c->SaveAs(output_pngfile.c_str());
+}
+
--- HistogramManager.h
+++ HistogramManager.h
@@ -3,17 +3,16 @@
 
 #include "TH1D.h"
 #include "TH2D.h"
+#include "TCanvas.h"
 #include <vector>
 #include "duneanaobj/StandardRecord/StandardRecord.h"
  
 struct HistogramCollection {
     TH1D* part_energy;
-    TH1D* interaction_vtx_R;
     TH1D* interaction_vtx_x;
     TH1D* interaction_vtx_y;
     TH1D* interaction_vtx_z;
     TH2D* tracks_Ecalo_len;
-    TH2D* tracks_Erange_len;
 };
 
 class HistogramManager {
@@ -23,6 +22,9 @@ public:
     static void fillRecoTracksHistograms(HistogramCollection& histograms, const caf::StandardRecord* sr, int nixn, int ntrack);
     static void fillRecoIxnHistograms(HistogramCollection& histograms, const caf::StandardRecord* sr, int nixn);
     static void writeHistograms(const HistogramCollection& histograms, const std::string& output_rootfile);
+    static void plotHistograms(const HistogramCollection& histograms, const std::string& output_folder);
+    static void plot1DHistogram(TH1D* hist, const std::string& output_folder, TCanvas *c);
+    static void plot2DHistogram(TH2D* hist, const std::string& output_folder, TCanvas *c);
 };
 
 #endif // HISTOGRAMMANAGER_H
