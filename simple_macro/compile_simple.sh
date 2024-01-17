59c3e36371fa555d403d5740c8bb0918b521da30
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-01-16 20:49:52 -0600

    Upgrade dependencies

--- simple_macro/compile_simple.sh
+++ simple_macro/compile_simple.sh
@@ -1,6 +1,5 @@
 #compile the CAF plotter
-
-g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` simple_caf_plotter.cc -o simple_plotter -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_01_00/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy
+g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` simple_caf_plotter.cc -o simple_plotter -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_02_01/include -I$SRPROXY_INC -L$DUNEANAOBJ_LIB -lduneanaobj_StandardRecordProxy -lduneanaobj_StandardRecord -lduneanaobj_StandardRecord_dict
 if [ $? -eq 0 ]; then
     echo "Compilation successful"
 else
