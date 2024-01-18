82293e18e2a5c2f8d652d356e689d7659097f447
sindhu-ku <kumaran.sindhujha@gmail.com>
2024-01-18 04:49:11 -0600

    rearrange for tutorial

--- old_intheworks/compile.sh
+++
@@ -1,7 +0,0 @@
-#compile the CAF plotter
-g++ -std=c++17 -O2 -g -pedantic -Wall `root-config --cflags --glibs` main.cc CAFPlotter.cc TreeLoader.cc HistogramManager.cc ApplyCuts.cc -o plotter -I/cvmfs/dune.opensciencegrid.org/products/dune/duneanaobj/v03_01_00/include
-if [ $? -eq 0 ]; then
-    echo "Compilation successful"
-else
-    echo "Compilation failed"
-fi
